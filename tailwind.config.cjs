79840677f4ebe055e8f6433982f73fb0b0a8331f
Hooi Jin Yee <jinyee0117@gmail.com>
2023-03-09 18:55:40 +0800

    update theme color

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,6 +2,13 @@
 module.exports = {
   content: ['./src/**/*.{html,js,svelte,ts}'],
   theme: {
+    colors: {
+      'rose100' : '#ffe4e6',
+      'rose200' : '#fecdd3',
+      'rose300' : '#fda4af',
+      'rose400' : '#fb7185',
+      'gray100' : '#f3f4f6'
+    },
     extend: {},
   },
   plugins: [require("daisyui")],
